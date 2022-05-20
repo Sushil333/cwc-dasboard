ca5e2a9ae26f78c14b886e2750ddab94d3e2a730
Sushil333 <Sushilbhardwaj705@gmail.com>
2022-05-20 05:28:17 +0000

    update

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -1,6 +1,6 @@
 import { Icon } from '@iconify/react';
 import pieChart2Fill from '@iconify/icons-eva/pie-chart-2-fill';
-import peopleFill from '@iconify/icons-eva/people-fill';
+// import peopleFill from '@iconify/icons-eva/people-fill';
 import shoppingBagFill from '@iconify/icons-eva/shopping-bag-fill';
 // ----------------------------------------------------------------------
 
